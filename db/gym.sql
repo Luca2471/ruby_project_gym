98615e188b68c006817668cbd269955cef7d5e07
Luca Sanz Charreun <lucasanz2471@gmail.com>
2019-12-16 21:59:43 +0000

    fixed some syntax errors

--- db/gym.sql
+++ db/gym.sql
@@ -5,19 +5,22 @@ DROP TABLE humans;
 CREATE TABLE memberships
 (
   id SERIAL PRIMARY KEY,
-  price INT,
+  price INT
 );
 
 CREATE TABLE humans
 (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(255),
-  surname VARCHAR(255)
+  surname VARCHAR(255),
+  age VARCAR(255)
 );
 
 CREATE TABLE Classes
 (
   id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  time VARCHAR(255),
+  humans_id INT REFERENCES humans(id),
   membership_id INT REFERENCES memberships(id),
-  humans_id INT REFERENCES humans(id)
 );
